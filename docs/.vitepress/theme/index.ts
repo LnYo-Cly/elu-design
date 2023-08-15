bf23b48ff4a99263113ade3be7576d5b2737ed6f
lnyocly <lnyocly@gmail.com>
2023-08-16 01:04:11 +0800

    docs: change some content

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,6 +1,6 @@
 import DefaultTheme from 'vitepress/theme';
 import { AntdTheme } from 'vite-plugin-vitepress-demo/theme';
-import EluDesign from '@elu-design/elu-design/index';
+import EluDesign from '@elu-design/components/index';
 import '@elu-design/theme-chalk/src/index.scss';
 import { EnhanceAppContext } from 'vitepress';
 import type { Theme } from 'vitepress';
